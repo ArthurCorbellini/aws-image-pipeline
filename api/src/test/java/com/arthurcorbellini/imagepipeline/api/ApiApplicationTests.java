afe254fbdbbf1e0039352bfb8106fb0b115c49f2
arthur <arthur.corbellini27@gmail.com>
2026-07-09 18:16:17 -0300

    chore: set up multi-module maven structure for api and worker services

---
+++ api/src/test/java/com/arthurcorbellini/imagepipeline/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurcorbellini.imagepipeline.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
